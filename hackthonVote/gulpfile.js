a7e55dfb87bd81f600bc5a0926aba7894ac453c2
Andrew <chenpoanandrew@gmail.com>
2015-04-25 13:58:16 +0800

    auto bundle now :"))

--- hackthonVote/gulpfile.js
+++ hackthonVote/gulpfile.js
@@ -7,17 +7,26 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var watch = require('gulp-watch');
+var batch = require('gulp-batch'); 
 
 //
 //tasks
 //
-gulp.task('bundle-js', function(){
+gulp.task('bundle-js', function () {
 	return gulp.src('private/js/*.js')
-		.pipe( concat('main.js') )
-		.pipe( rename( { suffix : '.min' } ) )
-		.pipe( uglify() )
-		.pipe( gulp.dest('public/javascripts') )
+		.pipe(concat('main.js'))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest('public/javascripts'))
 })
 
+gulp.task('watch', function () {
+    watch('private/js/*.js', batch(function () {
+        gulp.start('bundle-js').start('watch');
+    }));
+});
 
-gulp.task('default', ['bundle-js']);
+
+gulp.task('default', ['watch']);
