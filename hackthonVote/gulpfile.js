b69c876fbb30bdb3cf1899819e3b83d484206132
Andrew <chenpoanandrew@gmail.com>
2015-04-25 14:40:06 +0800

    wow~! live reload

--- hackthonVote/gulpfile.js
+++ hackthonVote/gulpfile.js
@@ -3,11 +3,26 @@
 //
 var gulp = require('gulp');
 
+//
+//plugin
+//
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var watch = require('gulp-watch');
-var batch = require('gulp-batch'); 
+var batch = require('gulp-batch');
+var server = require( 'gulp-develop-server' );
+var livereload = require( 'gulp-livereload' );
+
+var options = {
+    path: 'app.js'
+};
+ 
+var serverFiles = [
+    'app.js',
+    'routes/*.js'
+];
+
 
 //
 //tasks
@@ -22,11 +37,45 @@ gulp.task('bundle-js', function () {
 		.pipe(gulp.dest('public/javascripts'))
 })
 
-gulp.task('watch', function () {
-    watch('private/js/*.js', batch(function () {
-        gulp.start('bundle-js').start('watch');
-    }));
+
+//
+// run server & restart server
+//
+gulp.task( 'server:start', function() {
+    server.listen( options, livereload.listen );
+});
+ 
+gulp.task( 'server:restart', function() {
+    gulp.watch( [ 'app.js' ], server.restart );
 });
 
 
-gulp.task('default', ['watch']);
+//
+//watch
+//
+gulp.task('watch-js', function () {
+    watch('private/js/*.js', batch(function () {
+        gulp.start('bundle-js').start('watch-js');
+    }));
+});
+
+//gulp.task('watch-routes', function () {
+//    watch(['routes/*.js', 'app.js'], batch(function () {
+//        gulp.start('watch-routes').start('server-restart');
+//    }));
+//});
+
+
+//
+//default
+//
+gulp.task('default', ['watch-js', 'server:start'], function() {
+    
+    function restart( file ) {
+        server.changed( function( error ) {
+            if( ! error ) livereload.changed( file.path );
+        });
+    }
+ 
+    gulp.watch( serverFiles ).on( 'change', restart );
+});
--- hackthonVote/private/js/myMain.js
+++ hackthonVote/private/js/myMain.js
@@ -1,2 +1,12 @@
 var main = true;
 console.log('main', main);
+
+
+var tmp = function(){
+	
+	
+	//123123
+	
+	console.log('123');
+	
+}
