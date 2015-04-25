76afbf17a48d4125653cd26deebe2a56b73ae7dd
Andrew <chenpoanandrew@gmail.com>
2015-04-25 14:44:21 +0800

    remove test code for auto reload

--- hackthonVote/routes/index.js
+++ hackthonVote/routes/index.js
@@ -6,10 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
-router.get('/index', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-
 module.exports = router;
