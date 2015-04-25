f5bc6e0dc203a5f240d2f9386726d8fccf44f039
Andrew <chenpoanandrew@gmail.com>
2015-04-25 11:52:07 +0800

    remove gulp-nodemon, because need node 0.12

--- hackthonVote/gulpfile.js
+++ hackthonVote/gulpfile.js
@@ -6,9 +6,7 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-var nodemon = require('gulp-nodemon');
-
-var jshint = require('gulp-jshint');
+var watch = require('gulp-watch');
 
 //
 //tasks
@@ -21,21 +19,5 @@ gulp.task('bundle-js', function(){
 		.pipe( gulp.dest('public/javascripts') )
 })
 
-//gulp.task('lint', function () {
-//  gulp.src('./**/*.js')
-//    .pipe(jshint())
-//})
 
-gulp.task('develop', function () {
-  nodemon({ script: 'app.js'
-          , ext: 'html js jade'
-          , ignore: ['ignored.js']
-					, tasks: ['bundle-js']})
-    .on('restart', function () {
-      console.log('--------  restarted  --------');
-    });
-})
-
-
-gulp.task('default', [
-	'develop']);
+gulp.task('default', ['bundle-js']);
