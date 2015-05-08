fd0affd1f17f3bea75801c98a150bb8ee6e10342
webber_wu <webber_wu@hiiir.com>
2015-05-08 11:42:59 +0800

    加入 pre

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ var models = require('../models');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  console.log(req.route)
 	res.render('index', {
-		title: '我是首頁'
+		title: '我是首頁',
+    path: req.route
 	});
 });
 
