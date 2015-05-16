300fa53ee76e88e35ebac3768bf1aa647c3541a6
webber_wu <webber_wu@hiiir.com>
2015-05-16 20:13:50 +0800

    修改jade及拿掉多餘的code

---
+++ public/javascripts/main.js
@@ -0,0 +1,22 @@
+$(function(){
+  $( "#creatTeam" ).submit(function( event ) {
+ 
+    // Stop form from submitting normally
+    event.preventDefault();
+   
+    // Get some values from elements on the page:
+    var $form = $( this ),
+      // console.log( $form, '123')
+      term = $form.find( "#aaa" ).val(),
+      url = $form.attr( "action" );
+      console.log($form)
+    // Send the data using post
+    var posting = $.post( url, { aa: 'term' } );
+   
+    // Put the results in a div
+    posting.done(function( data ) {
+      var content = $( data ).find( "#content" );
+      $( "#result" ).empty().append( content );
+    });
+  });
+})
