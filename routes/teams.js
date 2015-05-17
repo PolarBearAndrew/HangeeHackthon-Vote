6b31b040af23fa26718c4cc36239fe119812bd1e
Andrew <chenpoanandrew@gmail.com>
2015-05-17 10:06:28 +0800

    fix jpg

--- routes/teams.js
+++ routes/teams.js
@@ -13,7 +13,7 @@ router.get('/create/test', function (req, res, next) {
   //new entity
   var team = new models.Team({
     name: 'WashWashSeelp',
-    imgURL: 'testImg.jpg',
+    imgURL: '',
 		projectName: 'hackthon vote',
 		projectDetail : 'For hackthon Vote ',
 		createDate : Date.now(),
